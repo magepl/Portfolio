aa8904d1606e83bdff9454b772605f2b0104fe4d
Matt <95616571+mungo88@users.noreply.github.com>
2022-05-03 20:52:37 +0100

    Add files via upload

---
+++ colorChange/script.js
@@ -0,0 +1,8 @@
+const colorChange = () => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = '#' + randomColor;
+  color.innerHTML = '#' + randomColor;
+};
+
+colorBtn.addEventListener('click', colorChange);
+colorChange();
